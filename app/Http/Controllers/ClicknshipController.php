b4a2a0b33ac65c592519cb44f8111bf96fe185b2
Dare Adewuyi <dare_adewuyi@yahoo.com>
2020-11-09 14:05:47 +0100

    testing

--- app/Http/Controllers/ClicknshipController.php
+++ app/Http/Controllers/ClicknshipController.php
@@ -76,8 +76,8 @@ class ClicknshipController extends Controller
                     //create Carrier
                     $shop = Auth::user();
                     $request = $shop->api()->rest('POST', '/admin/api/2020-10/carrier_services.json', ['carrier_service'
-                    =>['name'=>"ClicknShip Shipping"],"callback_url" => "lara-shopy.herokuapp.com/api/carriers",
-                    "service_discovery" => "true"
+                    =>['name'=>"ClicknShip Shipping","callback_url" => "lara-shopy.herokuapp.com/api/carriers",
+                    "service_discovery" => "true"]
                    ]);
                 dd($request);
                 return Redirect::back()->with('msg', 'Your store details are saved successfully');
